9ad6c0d010c74685762a0fbff7af515a633b0ae1
reneekaandorp <127418767+reneekaandorp@users.noreply.github.com>
2023-09-26 11:05:18 +0200

    Added GitHub url

--- src/App.js
+++ src/App.js
@@ -1,5 +1,22 @@
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <footer>
+        This project was coded by{" "}
+        <a href="https://silly-llama-8d5577.netlify.app" target="_blank">
+          Renée Kaandorp
+        </a>{" "}
+        and is{" "}
+        <a
+          href="https://github.com/reneekaandorp/react-weather-app"
+          target="_blank"
+        >
+          open-sourced on GitHub
+        </a>
+      </footer>
+    </div>
+  );
 }
