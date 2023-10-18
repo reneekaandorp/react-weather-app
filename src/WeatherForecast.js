5521c763927a8d76b13af394b4f27e9ad6d9c345
reneekaandorp <127418767+reneekaandorp@users.noreply.github.com>
2023-10-18 13:52:42 +0200

    Fixed forecast bug

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,22 +1,18 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./WeatherForecast.css";
 import axios from "axios";
 
 export default function WeatherForecast(props) {
   let [loaded, setLoaded] = useState(false);
   let [forecast, setForecast] = useState(null);
+
+  useEffect(() => {
+    setLoaded(false);
+  }, [props.city]);
+
   function handleResponse(response) {
     setForecast(response.data.daily);
     setLoaded(true);
-  }
-
-  function day() {
-    let date = new Date(forecast[0].time * 1000);
-    let day = date.getDay();
-
-    let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
-
-    return days[day];
   }
 
   if (loaded) {
@@ -24,6 +20,15 @@ export default function WeatherForecast(props) {
       <div className="WeatherForecast">
         <div className="row">
           {forecast.map(function (dailyForecast, index) {
+            function day() {
+              let date = new Date(dailyForecast.time * 1000);
+              let day = date.getDay();
+
+              let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+              return days[day];
+            }
+
             if (index < 6) {
               return (
                 <div className="col" key={index}>
